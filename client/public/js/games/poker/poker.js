ead82f124d47d037937f82fb417cd2ca01c28465
JeanCarlosVal <Jean_Valenzuela2021@outlook.com>
2022-10-30 21:46:15 -0400

    joining poker lobbies in progress

--- client/public/js/games/poker/poker.js
+++ client/public/js/games/poker/poker.js
@@ -15,5 +15,43 @@ var fold = document.getElementById("fold")
 var raise = document.getElementById("raise")
 
 check.addEventListener("click", e => {
-    poker.emit("check")
+    poker.emit("player-check")
+})
+
+fold.addEventListener("click", e => {
+    poker.emit("player-fold")
+})
+
+raise.addEventListener("click", e => {
+    poker.emit("player-raise")
+})
+
+var joinroom = document.getElementById("joinRoom")
+var seerooms = document.getElementById("rooms")
+
+joinroom.addEventListener("click", e =>{
+    const room = document.getElementById("room").value
+
+    poker.emit("join-room", room)
+})
+
+seerooms.addEventListener("click", e => {
+    poker.emit("see-rooms", )
+    document.getElementById("available-rooms").innerHTML = "";
+})
+
+poker.on("fetched-rooms", rooms => {
+    console.log(rooms)
+
+    rooms.forEach(element => {
+        poker.emit("check-room", element)
+    });
+})
+
+poker.on('is-room-available', (availability, room) => {
+    if(availability){
+        var p = document.createElement('p')
+        p.innerText = room
+        document.getElementById("available-rooms").appendChild(p)
+    }
 })
--- server/server.js
+++ server/server.js
@@ -10,6 +10,32 @@ io.on("connection" , socket => {
     console.log("root connection: " +socket.id)
 })
 
+//When user connects do this
 pokerIo.on("connection" , socket => {
+    //When connected it automatically sets a room for the user using the user id this code below leaves it then it logs the user id
+    socket.leave(socket.id)
     console.log("poker connection: " +socket.id)
+
+    socket.on("join-room" , room => {
+        socket.join(room)
+        console.log("joined room: " + room)
+    })
+
+    socket.on("see-rooms", eval => {
+        const rooms = Array.from(io.of("/poker").adapter.rooms.keys())
+
+        pokerIo.emit('fetched-rooms', rooms)
+    })
+
+    socket.on("check-room", room => {
+        const map = io.of("/poker").adapter.rooms
+        const users = Array.from(map.get(room))
+        console.log(room)
+
+        if(users.length > 6){
+            pokerIo.emit('is-room-available', false, room)
+        }else{
+            pokerIo.emit('is-room-available', true, room)
+        }
+    })
 })
